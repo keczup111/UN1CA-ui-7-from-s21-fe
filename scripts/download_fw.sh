9e8c5dc36c72a4ce70d88023467247ddb567349c
keczup111 <133388559+keczup111@users.noreply.github.com>
2025-09-25 17:50:53 +0200

    Update download_fw.sh

--- scripts/download_fw.sh
+++ scripts/download_fw.sh
@@ -1,50 +1,71 @@
-#!/usr/bin/env bash
 
+#!/usr/bin/env bash
+#
 # Copyright (C) 2023 Salvo Giangreco
-# License: GNU GPL v3 or later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# shellcheck disable=SC2162
 
 set -e
 
-# === CONFIGURATION ===
-export ODIN_DIR="$HOME/firmwares"
-
-# Source: Galaxy S23 Ultra (Snapdragon EU)
-SOURCE_FIRMWARE="SM-S918B/EUX/354721884463723"
-
-# Target: Galaxy S21 FE (Snapdragon EU)
-TARGET_FIRMWARE="SM-G990B2/EUX/354721884463723"
-
-# Additional firmwares (optional, colon-separated)
-SOURCE_EXTRA_FIRMWARES="SM-S918B/EUX/MODEM:SM-S918B/EUX/CSC:SM-S918B/EUX/BL"
-TARGET_EXTRA_FIRMWARES=""
-
-# === FUNCTIONS ===
-GET_LATEST_FIRMWARE() {
+# [
+GET_LATEST_FIRMWARE()
+{
     curl -s --retry 5 --retry-delay 5 "https://fota-cloud-dn.ospserver.net/firmware/$REGION/$MODEL/version.xml" \
         | grep latest | sed 's/^[^>]*>//' | sed 's/<.*//'
 }
 
-DOWNLOAD_FIRMWARE() {
-    local PDR="$(pwd)"
+DOWNLOAD_FIRMWARE()
+{
+    local PDR
+    PDR="$(pwd)"
+
     cd "$ODIN_DIR"
+    { samfirm -m "$MODEL" -r "$REGION" -i "$IMEI" > /dev/null; } 2>&1 \
+        && touch "$ODIN_DIR/${MODEL}_${REGION}/.downloaded" \
+        || exit 1
+    [ -f "$ODIN_DIR/${MODEL}_${REGION}/.downloaded" ] && {
+        echo -n "$(find "$ODIN_DIR/${MODEL}_${REGION}" -name "AP*" -exec basename {} \; | cut -d "_" -f 2)/"
+        echo -n "$(find "$ODIN_DIR/${MODEL}_${REGION}" -name "CSC*" -exec basename {} \; | cut -d "_" -f 3)/"
+        echo -n "$(find "$ODIN_DIR/${MODEL}_${REGION}" -name "CP*" -exec basename {} \; | cut -d "_" -f 2)"
+    } >> "$ODIN_DIR/${MODEL}_${REGION}/.downloaded"
 
-    echo "- Downloading firmware for $MODEL with CSC $REGION..."
-    samfirm -m "$MODEL" -r "$REGION" -i "$IMEI" || exit 1
-
-    LATEST_VERSION=$(GET_LATEST_FIRMWARE)
-    echo "$LATEST_VERSION" > "$ODIN_DIR/${MODEL}_${REGION}/.version"
-
+    echo ""
     cd "$PDR"
 }
 
-# === FIRMWARE LIST PROCESSING ===
 FIRMWARES=( "$SOURCE_FIRMWARE" "$TARGET_FIRMWARE" )
-IFS=':' read -ra SOURCE_EXTRA <<< "$SOURCE_EXTRA_FIRMWARES"
-IFS=':' read -ra TARGET_EXTRA <<< "$TARGET_EXTRA_FIRMWARES"
-FIRMWARES+=( "${SOURCE_EXTRA[@]}" "${TARGET_EXTRA[@]}" )
+IFS=':' read -a SOURCE_EXTRA_FIRMWARES <<< "$SOURCE_EXTRA_FIRMWARES"
+if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
+    for i in "${SOURCE_EXTRA_FIRMWARES[@]}"
+    do
+        FIRMWARES+=( "$i" )
+    done
+fi
+IFS=':' read -a TARGET_EXTRA_FIRMWARES <<< "$TARGET_EXTRA_FIRMWARES"
+if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
+    for i in "${TARGET_EXTRA_FIRMWARES[@]}"
+    do
+        FIRMWARES+=( "$i" )
+    done
+fi
+# ]
 
-# === OPTIONS ===
 FORCE=false
+
 while [ "$#" != 0 ]; do
     case "$1" in
         "-f" | "--force")
@@ -56,52 +77,39 @@ while [ "$#" != 0 ]; do
             exit 1
             ;;
     esac
+
     shift
 done
 
 mkdir -p "$ODIN_DIR"
 
-# === FIRMWARE DOWNLOAD ===
-for i in "${FIRMWARES[@]}"; do
-    MODEL=$(echo -n "$i" | cut -d "SM-S918B" -f 1)
-    REGION=$(echo -n "$i" | cut -d "EUX" -f 2)
-    IMEI=$(echo -n "$i" | cut -d "354721884463723" -f 3)
+for i in "${FIRMWARES[@]}"
+do
+    MODEL=$(echo -n "$i" | cut -d "/" -f 1)
+    REGION=$(echo -n "$i" | cut -d "/" -f 2)
+    IMEI=$(echo -n "$i" | cut -d "/" -f 3)
 
-    if [ -z "$MODEL" ] || [ -z "$REGION" ] || [ -z "$IMEI" ]; then
-        echo "Error: Could not determine model, region or IMEI from '$i'. Skipping."
-        continue
-    fi
-
-    VERSION_FILE="$ODIN_DIR/${MODEL}_${REGION}/.version"
-    if [ -f "$VERSION_FILE" ]; then
-        LATEST_VERSION=$(GET_LATEST_FIRMWARE)
-        [ -z "$LATEST_VERSION" ] && continue
-        if [[ "$LATEST_VERSION" != "$(cat "$VERSION_FILE")" ]]; then
+    if [ -f "$ODIN_DIR/${MODEL}_${REGION}/.downloaded" ]; then
+        [ -z "$(GET_LATEST_FIRMWARE)" ] && continue
+        if [[ "$(GET_LATEST_FIRMWARE)" != "$(cat "$ODIN_DIR/${MODEL}_${REGION}/.downloaded")" ]]; then
             if $FORCE; then
-                echo "- Updating firmware for $MODEL with CSC $REGION..."
+                echo "- Updating $MODEL firmware with $REGION CSC..."
                 rm -rf "$ODIN_DIR/${MODEL}_${REGION}" && DOWNLOAD_FIRMWARE
             else
-                echo "- Firmware for $MODEL with CSC $REGION is outdated."
-                echo "  Use --force to download the latest version."
+                echo    "- $MODEL firmware with $REGION CSC already downloaded"
+                echo    "  A newer version of this device's firmware is available."
+                echo -e "  To download, clean your Odin firmwares directory or run this cmd with \"--force\"\n"
                 continue
             fi
         else
-            echo "- Firmware for $MODEL with CSC $REGION is up to date. Skipping..."
+            echo -e "- $MODEL firmware with $REGION CSC already downloaded\n"
             continue
         fi
     else
-        DOWNLOAD_FIRMWARE
+        echo "- Downloading $MODEL firmware with $REGION CSC..."
+        rm -rf "$ODIN_DIR/${MODEL}_${REGION}" && DOWNLOAD_FIRMWARE
     fi
 done
 
-# === SUMMARY ===
-echo ""
-echo "Downloaded firmware versions:"
-for i in "${FIRMWARES[@]}"; do
-    MODEL=$(echo -n "$i" | cut -d "/" -f 1)
-    REGION=$(echo -n "$i" | cut -d "/" -f 2)
-    VERSION_FILE="$ODIN_DIR/${MODEL}_${REGION}/.version"
-    echo "- $MODEL ($REGION): $(cat "$VERSION_FILE" 2>/dev/null || echo "not downloaded")"
-done
-
 exit 0
+
